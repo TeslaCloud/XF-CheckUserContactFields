7b685c7091d62f40733a18c50d58f5aaa7f270bd
Nikita Kolesnikov <nikitos@mv-i.ru>
2023-12-10 01:53:19 +0300

    Update Forum.php

--- XF/Pub/Controller/Forum.php
+++ XF/Pub/Controller/Forum.php
@@ -9,37 +9,36 @@ namespace TC\CUCF\XF\Pub\Controller;
 
 use XF;
 use XF\Mvc\ParameterBag;
-use XF\Mvc\Reply\View;
+use XF\Mvc\Reply\AbstractView;
 
 class Forum extends XFCP_Forum
 {
-    public function actionPostThread(ParameterBag $params)
-    {
-        $reply = parent::actionPostThread($params);
+	/**
+	 * @param ParameterBag $params
+	 *
+	 * @return AbstractView
+	 */
+	public function actionPostThread(ParameterBag $params)
+	{
+		$reply = parent::actionPostThread($params);
+        
+		if (
+			!($reply instanceof View)
+			|| !in_array($params->node_id, XF::options()->tc_cucf_include_forums)
+			|| XF::visitor()->hasPermission('forum', 'tc_cucf_bypass')
+		)
+		{
+			return $reply;
+		}
 
-        if ($reply instanceof View)
-        {
-            if (in_array($params->node_id, XF::options()->tc_cucf_include_forums))
-            {
-                if (XF::visitor()->hasPermission('forum', 'tc_cucf_bypass'))
-                {
-                    return $reply;
-                }
+		foreach (XF::options()->tc_cucf_contactFields as $fieldName)
+		{
+			if (XF::visitor()->Profile->custom_fields[$fieldName])
+			{
+				return $reply;
+			}
+		}
 
-                foreach (XF::options()->tc_cucf_contactFields as $fieldName => $def)
-                {
-                    $value = XF::visitor()->Profile->custom_fields[$def];
-
-                    if ($value) // checking if more than one contact field filled
-                    {
-                        return $reply;
-                    }
-                }
-
-                return $this->view(null, 'tc_cucf_view');
-            }
-        }
-
-        return $reply;
-    }
+		return $this->view('TC\CUCF:Forum\PostThread\Contacts', 'tc_cucf_view');
+	}
 }
